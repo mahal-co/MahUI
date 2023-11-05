89838bbdc3bfe5ce80458786c431b4807ed95e7a
Isa Memmedli <i.a.memmedli@gmail.com>
2023-11-05 11:47:51 +0400

    Init MahUIApp

---
+++ demo/MahUIApp/MahUIApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MahUIApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ demo/MahUIApp/MahUIApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MahUIApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
